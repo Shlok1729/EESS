025d966ca6d26a8bfe052e71a276d57b37211db5
Shlok Goyal <shlokgoyal1279@gmail.com>
2026-07-08 19:03:38 +0530

    vercel analytics

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout = () => {
   return (
     <div className="font-sans flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow pt-20">
+        <Analytics />
         <Outlet />
       </main>
       <Footer />
